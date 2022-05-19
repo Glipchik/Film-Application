8d25653ecfb272c09878826855e43478e7616333
Glibchik <81156189+Glipchik@users.noreply.github.com>
2022-05-19 09:59:15 +0300

    Add first part of application

---
+++ FilmLibrary.SharedData/TestsData/UnitTestsConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace FilmLibrary.SharedData
+{
+    public static class UnitTestsConstants
+    {
+        public const string TestsConnectionString = "Data Source=H-BIAROZKIN\\MSSQLSERVER2;Initial Catalog=ConsoleAppDb;Integrated Security=True;";
+    }
+}
